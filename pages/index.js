679ee93888eb9ffeed63fba8feb4e1142e9d2474
Dmitriy Loktev <dmitry.loktev@jetbrains.com>
2021-11-04 18:59:09 +0300

    Initial next.js + tailwind

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div class="py-12 bg-white">
+      <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        Prepare for better potomushto
+      </p>
+    </div>
+  )
+}
